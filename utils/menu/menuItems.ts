20c2c15cbde3ddbb9ff555fc0eea5547355f9d06
Amelia <nawrotamelia3@gmail.com>
2025-06-15 12:12:57 +0200

    correction meunItems.ts

--- utils/menu/menuItems.ts
+++ utils/menu/menuItems.ts
@@ -1,10 +1,11 @@
-import { BottomMenuItem } from '@/components/atoms/BottomMenu/types'; // jeśli masz typ
+
 import { NavigationProp } from '@react-navigation/native';
 import { RootStackParamList } from '@/types/navigation';
+import {MenuItem} from "@/components/ui/molecules/Menu/Bottom/types";
 
 export const getMenuItems = (
     navigation: NavigationProp<RootStackParamList>
-): BottomMenuItem[] => [
+): MenuItem[] => [
     { label: 'Home', icon: 'home-outline', onPress: () => navigation.navigate('HomeScreen') },
     { label: 'Profile', icon: 'person-outline', onPress: () => navigation.navigate('ProfileScreen') },
     { label: 'History', icon: 'time-outline', onPress: () => navigation.navigate('HistoryScreen') },
